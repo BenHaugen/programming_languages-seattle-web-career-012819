a756d20f80c84811d66953b6c0981ed358e49d2f
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-26 18:38:14 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,10 +3,9 @@ def reformat_languages(languages)
   hash = {}
   languages.each do |style, info|
     info.each do |language, value|
-      value.each do |type, type_value|
         binding.pry
         hash << language
-      end
+      
     end
   end
 end

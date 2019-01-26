4a1c75bc63978f7bf7d38baa0ea13f2978bd2b0f
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-26 19:01:39 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,7 @@ def reformat_languages(languages)
   languages.each do |style, language|
     language.each do |info, type_info|
       if hash[info]
-          hash[info][:type] << :style
+          hash[info][:style] << style
       end
     end
   end
